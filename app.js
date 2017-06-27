969b98f4a63a72b9eac9c9dc804d3e2640806965
Neilson Gamble <neilgamb@neilsons-mbp.attlocal.net>
2017-06-27 10:42:39 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+window.addEventListener('load', function() {
+
+    let currentQuote = 0;
+
+    let quotes = [
+        {quote: "DON'T YOU THINK THAT IF I WERE WRONG, I'D KNOW IT?", 
+         author: "SHELDON COOPER",
+         likes: 423,
+        },
+        {quote: "I DON'T KNOW HIM WELL ENOUGH FOR A STOP AND CHAT.", 
+         author: "LARRY DAVID",
+         likes: 572,
+        },
+        {quote: "I'M AFRAID I JUST BLUE MYSELF", 
+         author: "TOBIAS FüNKE",
+         likes: 419,
+        },
+        {quote: "THAT'S WHAT SHE SAID", 
+         author: "MICHAEL SCOTT",
+         likes: 742,
+        },
+        {quote: "D'OH!", 
+         author: "HOMER SIMPSON",
+         likes: 610,
+        },
+    ];
+
+
+});
